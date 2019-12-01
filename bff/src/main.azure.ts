0f9c938e8a892b5eae9b68536a08c2935bccafd6
Kazuyuki Miyake (ZEN) <k-miyake@zengeeks.io>
2019-12-01 23:46:24 +0900

    Enabled azure function native routing

--- bff/src/main.azure.ts
+++ bff/src/main.azure.ts
@@ -1,11 +1,12 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AzureHttpRouter } from '@nestjs/azure-func-http';
 
 export async function createApp(): Promise<INestApplication> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new AzureHttpRouter());
   app.setGlobalPrefix('api');
-  
+
   await app.init();
   return app;
 }
